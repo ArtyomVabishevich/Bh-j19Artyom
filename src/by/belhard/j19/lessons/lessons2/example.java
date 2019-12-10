0a31fe9ded4797f41463047618c1c361ec4cea35
andrey-zubov <taac@mail.ru>
2019-12-10 20:40:59 +0300

    init

---
+++ src/by/belhard/j19/lessons/lessons2/example.java
@@ -0,0 +1,12 @@
+package by.belhard.j19.lessons.lessons2;
+
+public class example {
+    public static void main(String[] args) {
+
+        int it= 0;
+        System.out.println(it++);
+        System.out.println(++it);
+
+
+    }
+}
