13385654bdfd34dc50251928f8edd083855137cd
ArtyomVabishevich <avabishevich@mail.ru>
2019-12-12 16:47:16 +0300

    \\

--- src/by/belhard/j19/lessons/lessons2/example.java
+++ src/by/belhard/j19/lessons/lessons2/example.java
@@ -2,11 +2,23 @@ package by.belhard.j19.lessons.lessons2;
 
 public class example {
     public static void main(String[] args) {
-
-        int it= 0;
-        System.out.println(it++);
-        System.out.println(++it);
-
-
+        byte b=2;
+        short sh=22;
+        char ch=222;
+        int i=2222;
+        long lg=22222;
+        double dl=22.2;
+        float ft=222.2f;
+        boolean bl=true;
+        boolean bn=false;
+        System.out.println(sh-b);
+        System.out.println(ch/sh+b);
+        if(i == 2222)
+        System.out.println(i=2222);
+        else if(i>2222);
+        System.out.println("i>2222");
+ if(i<2222);
+        System.out.println("i<2222");
+        System.out.println(b*i);
     }
 }
