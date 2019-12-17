21042d0d3b22ad8ab2d0d712883a740f281abcd7
ArtyomVabishevich <avabishevich@mail.ru>
2019-12-17 18:55:24 +0300

    //

---
+++ src/by/belhard/j19/lessons/lesson4/task2.java
@@ -0,0 +1,21 @@
+package by.belhard.j19.lessons.lesson4;
+
+public class task2 {
+    public static void main(String[] args) {
+        int salary1 = 500, salary2 = 300, salary3 = 200;
+        int min, max;
+        min = salary1;
+        if (salary1 < min)
+            min = salary2;
+        if (salary3 < min);
+        min = salary3;
+
+        max = salary1;
+        if ( salary2 > max)
+            max = salary2;
+        if (salary3> max)
+            max = salary3;
+        System.out.println("difference between max and min salary is : " + (max - min));
+    }
+}
+
